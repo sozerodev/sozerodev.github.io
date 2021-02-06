a59d9350be0a7380bc2b450ceb9a588f1ec73c90
sozerodev <sozerodev@gmail.com>
2021-02-06 13:35:01 +0900

    change profile

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `soyoung's devLog`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
+  author: `soyoung kim`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://sozerodev.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `sozerodev`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
